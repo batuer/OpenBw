8956618ba2b02624c3fe30cd1172ebe0ce55171c
18609573806@126.com <18609573806@126.com>
2019-03-09 21:58:28 +0800

    OpenBw

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.lang.reflect.Method;
 import java.util.Scanner;
 
 public class Main {
@@ -14,43 +15,75 @@ public class Main {
 
     public static void load(String fileName) {
         File file = new File(fileName);
-
         if (!file.exists()) {
             System.err.println("不存在该文件: " + fileName);
             return;
         }
-        File errorFile = new File(fileName + "_err.txt");
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file));
-            errorFile.createNewFile();
-            PrintWriter writer = new PrintWriter(new FileWriter(errorFile));
             String lineTxt = null;
-            boolean hasError = false;
             while ((lineTxt = reader.readLine()) != null) {
-                if (!browserUrl(lineTxt)) {
-                    hasError = true;
-                    writer.println(lineTxt);
-                }
+                browserUrlWindow(lineTxt);
             }
             reader.close();
-            writer.close();
-            if (!hasError) {
-                errorFile.delete();
-            }
         } catch (Exception e) {
-            System.err.println(e.toString());
+            System.err.println(":--:" + e.toString());
         }
     }
 
 
-    public static boolean browserUrl(String url) {
-        try {
-            Runtime.getRuntime().exec("cmd   /c   start  " + url);
-        } catch (Exception e) {
-            System.err.println(e);
-            return false;
+    public static void browserUrlWindow(String url) throws Exception {
+//        if (java.awt.Desktop.isDesktopSupported()) {
+//            // 创建一个URI实例
+//            java.net.URI uri = java.net.URI.create(url);
+//            // 获取当前系统桌面扩展
+//            java.awt.Desktop dp = java.awt.Desktop.getDesktop();
+//            // 判断系统桌面是否支持要执行的功能
+//            if (dp.isSupported(java.awt.Desktop.Action.BROWSE)) {
+//                // 获取系统默认浏览器打开链接
+//                dp.browse(uri);
+//            }
+//        } else {
+//            System.err.println("不支持浏览器!");
+//        }
+
+
+        // 获取操作系统的名字
+        String osName = System.getProperty("os.name", "");
+        System.out.println("OsName:" + osName);
+        if (osName.startsWith("Mac OS")) {
+            // 苹果的打开方式
+            Class fileMgr = Class.forName("com.apple.eio.FileManager");
+            Method openURL = fileMgr.getDeclaredMethod("openURL", new Class[]{String.class});
+            openURL.invoke(null, new Object[]{url});
+        } else if (osName.startsWith("Windows")) {
+            // windows的打开方式。
+            Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+        } else if (osName.startsWith("Linux")) {
+            // 这个值在上面已经成功的得到了一个进程。
+            Process exec = Runtime.getRuntime().exec(new String[]{"google-chrome", url});
+            OutputStream os = exec.getOutputStream();
+            InputStream errorStream = exec.getErrorStream();
+            InputStream inputStream = exec.getInputStream();
+            int errAvailable = errorStream.available();
+            byte[] errBytes = new byte[errAvailable];
+            if (errorStream.read(errBytes) != -1) {
+                String errStr = new String(errBytes);
+                System.out.println(":errStr--:" + errStr + ":");
+            }
+            int available = inputStream.available();
+
+            byte[] bytes = new byte[available];
+            if (inputStream.read(bytes) != -1) {
+                String str = new String(errBytes);
+                System.out.println(":--:" + str + ":");
+            }
+            System.out.println(errAvailable + ":----:" + available);
+            os.close();
+            errorStream.close();
+            inputStream.close();
         }
-        return true;
+
     }
 
 }
